bd70caa1d5a252b74e9877bcd68facc96292e1cb
Gr Code Club <135416047+GrCodeClub@users.noreply.github.com>
2023-06-09 11:51:20 +0300

    Create cout_cin.cpp

---
+++ Εντολές/cout_cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    cout << "Παρακαλώ εισάγετε έναν ακέραιο αριθμό: ";
+    cin >> number;
+    cout << "Έχετε εισάγει τον αριθμό: " << number << endl;
+
+    return 0;
+}
