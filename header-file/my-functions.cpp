9dbff4389ee51a187efae9099fb71699131238c0
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-06-25 13:59:40 +0300

    Create my-functions.cpp

---
+++ header-file/my-functions.cpp
@@ -0,0 +1,11 @@
+// my_functions.cpp
+#include "my-functions.h"
+#include <iostream>
+
+void printHello() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
